834ed62c88689ce22953703be5cacd310aa426e5
Nikhil Seth <nseth435@gmail.com>
2023-12-11 19:29:38 +0530

    Pattern

---
+++ Q7.cpp
@@ -0,0 +1,23 @@
+/*
+1
+21
+321
+4321*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int c,i;
+    for(i=1;i<=4;i++)
+    {
+        c=i;
+        while(c>0)
+        {
+            cout<<c;
+            c=c-1;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
